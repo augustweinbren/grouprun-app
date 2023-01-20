4d3a763ab65e72041420333a2130988db58523da
August Weinbren <weinbrenaugust@gmail.com>
2023-01-20 16:10:20 +0000

    Unnecessary things removed from HomeList

--- app/lib/model/homelist.dart
+++ app/lib/model/homelist.dart
@@ -22,13 +22,5 @@ class HomeList {
       imagePath: 'assets/hotel/hotel_booking.png',
       navigateScreen: HotelHomeScreen(),
     ),
-    HomeList(
-      imagePath: 'assets/fitness_app/fitness_app.png',
-      navigateScreen: FitnessAppHomeScreen(),
-    ),
-    HomeList(
-      imagePath: 'assets/design_course/design_course.png',
-      navigateScreen: DesignCourseHomeScreen(),
-    ),
   ];
 }
